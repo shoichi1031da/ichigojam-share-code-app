288fd78d8f6c464e18bbf5b4bf5e87e3c2ac343b
Shoichi Nakamura <41470674+shoichi1031da@users.noreply.github.com>
2022-07-24 22:54:24 +0900

    fix first version

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json()); 
 
 const cors = require("cors");
     app.use(cors({
@@ -9,22 +10,46 @@ const cors = require("cors");
     }));
 
 const PORT = process.env.PORT || 3000;
-let code = [];
+let CODE = [];
 
 app.use(express.static("public"));
 
+// http.createServer((req,res) => {
+//     if(req.method === "GET"){
+//         if(!req.body){
+//             console.log("GETリクエスト");
+//             res.end(html);
+//         }
+//         else if(!id && req.query.id == app.get("ID")){
+//             res.send(app.get("CODE"));
+//         }
+
+//     } else if(req.method === "POST"){
+//         console.log("CODE:" + req.body.CODE);
+//         console.log("ID:" + req.query.ID);
+//         app.set("CODE",req.body.CODE);
+//         app.set("ID",req.query.ID);
+//         res.send(req.query.CODE);
+//     }
+
+// }).listen(PORT);
 
 app.post('/', (req, res) => {
-    console.log(req.body.command);
-    app.set("code",req.body.command);
-    code[0] = req.body.command;
-    res.send(code[0]);
+    console.log(req.body);
+    console.log("postID:" + req.body.ID);
+    console.log("postCODE:" + req.body.CODE);
+    app.set("ID",req.body.ID);
+    app.set("CODE",req.body.CODE);
+    res.send(req.body.CODE);
     
 });
 
 app.get('/', (req, res) => {
-    console.log("appget:" + app.get("code"));
-    if(req.query.id == 1)res.send(app.get("code"));
+    console.log("GET");
+    if(req.query.id != undefined && req.query.id == app.get("ID")){
+        console.log(app.get("CODE"));
+        res.send(app.get("CODE"));
+    }
     else res.render("index.ejs");
 });
 
